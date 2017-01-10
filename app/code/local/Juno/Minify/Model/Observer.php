970121293d0c73c709d726b9e79a0a8fd5828b0a
Hieu Nguyen <hieu@junowebdesign.com>
2017-01-10 12:00:04 +0700

    fix issue wrong exist minified file check

--- app/code/local/Juno/Minify/Model/Observer.php
+++ app/code/local/Juno/Minify/Model/Observer.php
@@ -34,7 +34,7 @@ class Juno_Minify_Model_Observer
             }
             $hashData = $this->_getMinifiedData($file);
             $minifiedFile = Mage::helper('juno_minify')->getMinifiedFile($file);
-            if ($minifiedFile && !file_exists($minifiedFile)) {
+            if (!$minifiedFile) {
                 $result[] = $file;
                 continue;
             }
